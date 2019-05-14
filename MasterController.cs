e84e808fd96960711277f8efacf46984200eb53d
Dhananjay Shukla <dhananjayshukla2012@gmail.com>
2019-05-14 09:43:24 +0530

    Add files via upload

---
+++ MasterController.cs
@@ -0,0 +1,27 @@
+﻿using MvcApplication4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class MasterController : Controller
+    {
+
+        DataLayer db = new DataLayer();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public SelectList LoadCountry()
+        {
+            var lst = db.GetCountry();
+            System.Web.Mvc.SelectList lst2 = new SelectList(lst, "Value", "Name");
+            return lst2;
+        }
+
+    }
+}
---
+++ RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using MvcApplication4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication4.Models;
+
+namespace MvcApplication4.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        //
+        // GET: /Registration/
+        DataLayer db = new Models.DataLayer();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student model)
+        {
+            return View();
+        }
+        public JsonResult State(string CountryId)
+        {
+            var lst = db.GetCountry();
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
